38ca754c3ee2cfdb03428735ce66773d8e631281
Will Bamford <will.bamford@moonpig.com>
2022-07-13 13:23:33 +0100

    Test

--- next.config.js
+++ next.config.js
@@ -1,7 +1,31 @@
+// const withTM = require("next-transpile-modules")([
+//   "@nextjs-alias-test-lib/testpkg-internal",
+// ]);
+
+const target = process.env.TARGET;
+console.log(`Running with target: ${target}`);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  target,
   reactStrictMode: true,
   swcMinify: true,
-}
 
-module.exports = nextConfig
+  webpack: (config, context) => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        // alias: {
+        //   ...config.resolve.alias,
+        //   "@nextjs-alias-test-lib/testpkg": "@nextjs-alias-test-lib/testpkg-2",
+        // },
+      },
+    };
+  },
+};
+
+// this works
+// module.exports = withTM(nextConfig);
+
+module.exports = nextConfig;
